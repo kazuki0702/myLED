43230d276debb09723dfe717d57f286b30a1414a
kazuki0702 <59689103+kazuki0702@users.noreply.github.com>
2020-01-09 19:55:12 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -1,3 +1,23 @@
+/*
+ * myled.c
+ *
+ * Copyright (C) 2019 Ryuichi Ueda <ryuichiueda@gmail.com>
+ * Copyright (C) 2019 Kazuki Yoshimura <kazuki.scr.77@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
@@ -33,7 +53,6 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 	gpio_base[10] = 1 << 25;
         msleep(1000);
 	}
-    //回数点滅
     else if(c == '2'){
 	gpio_base[7] = 1 << 25;
 	msleep(2000);
